0e147ece4323c716b9a441803b0cfeaf20211514
ralferlebach <ralf.erlebach@gmx.de>
2026-03-27 22:37:44 +0100

    Update fixing style issues

--- db/services.php
+++ db/services.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
 $functions = [
     'local_stackmathgame_get_quiz_config' => [
         'classname' => 'local_stackmathgame\\external\\get_quiz_config',
